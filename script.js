aad5bfc6351a7b93a42a1103ab8601fdf3ba8cb6
shienna7 <125837545+shienna7@users.noreply.github.com>
2024-10-18 21:31:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Web-Developer", "Coder"],
+        strings: ["Student", "Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Web-Developer", "Coder"],
+        strings: ["Student", "Coder"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
